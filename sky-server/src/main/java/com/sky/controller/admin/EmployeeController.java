7b0b163eda7a0709bc5ca6b77362b4e4e75ecc50
zhang <@ZhangMingWei>
2023-12-13 17:03:11 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -92,4 +92,13 @@ public class EmployeeController {
         return Result.success(pageResult);
     }
 
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("员工id{}：状态{}", id, status);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
+
+
 }
