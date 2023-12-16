c13c913f7a5434dadac12d55eabff3feaccdcfad
zhang <@ZhangMingWei>
2023-12-16 21:45:26 +0800

    菜品的分页查询

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -20,4 +23,8 @@ public interface DishMapper {
 
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+
+
+    //菜品的分页查询
+    Page<DishVO> pageQuery(DishPageQueryDTO pageQueryDTO);
 }
